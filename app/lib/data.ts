b9f733b57dd48325b1f8d004efd106b4a9ff531c
enchufin <lincografic@gmail.com>
2024-06-20 13:38:44 +0200

    Chapter 9 in progress

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -14,13 +14,13 @@ import { unstable_noStore as noStore } from 'next/cache';
 export async function fetchRevenue() {
   // Add noStore() here to prevent the response from being cached.
   // This is equivalent to in fetch(..., {cache: 'no-store'}).
-  //noStore();
+  noStore();
   try {
     // Artificially delay a response for demo purposes.
     // Don't do this in production :)
 
     console.log('Fetching revenue data...');
-    await new Promise((resolve) => setTimeout(resolve, 1000)); 
+    await new Promise((resolve) => setTimeout(resolve, 3000)); 
 
     const data = await sql<Revenue>`SELECT * FROM revenue`;
 

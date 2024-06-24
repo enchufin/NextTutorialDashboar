fd360e422456eba2b4660938a79b4a1f677c5319
enchufin <lincografic@gmail.com>
2024-06-24 18:34:37 +0200

    Chapter 12 | 4. Validate and prepare the data

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -2,28 +2,47 @@
 
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
 import { useSearchParams , useRouter, usePathname } from 'next/navigation';
+import { useDebouncedCallback } from 'use-debounce';
 
 export default function Search({ placeholder }: { placeholder: string }) {
   const searchParams = useSearchParams();
   const pathname = usePathname();
   const { replace } = useRouter(); //llamada al método replace de useRouter
 
-  function handleSearch(term: string){
+/*   function handleSearch(term: string){
+    console.log(`Searching... ${term}`);
     //instanciamos los parametros de la URL
     const params = new URLSearchParams(searchParams);
       if(term){
-        params.set('query', term);
+        params.set('query', term); */
         /* Si term tiene un valor, se establece el parámetro query en la URL con este valor. Esto significa que si el usuario ingresa un término de búsqueda, se añadirá (o actualizará) el parámetro query en la URL con el término proporcionado. */       
-      }else {
+/*       }else { */
         /* se elimina el parámetro query de la URL. Esto es útil para limpiar la búsqueda cuando el usuario borra el término de búsqueda, asegurando que la URL no contenga parámetros de búsqueda innecesarios. */
+ /*        params.delete('query');
+      }
+      replace(`${pathname}?${params.toString()}`); 
+
+       console.log(term); // pinto en consola por curiosidad
+    console.log(`${pathname}?${params.toString()}`);
+  }
+      */
+      
+    // Inside the Search Component...
+    const handleSearch = useDebouncedCallback((term) => {
+      console.log(`Searching... ${term}`);
+    
+      const params = new URLSearchParams(searchParams);
+      params.set('page', '1');
+      if (term) {
+        params.set('query', term);
+      } else {
         params.delete('query');
       }
       replace(`${pathname}?${params.toString()}`);
+    }, 300);
 
-    console.log(term); // pinto en consola por curiosidad
-    console.log(`${pathname}?${params.toString()}`);
-  }
-  
+
+   
   return (
     <div className="relative flex flex-1 flex-shrink-0">
       <label htmlFor="search" className="sr-only">
